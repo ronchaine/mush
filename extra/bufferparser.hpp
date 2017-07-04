ad8540b5dd9b4ee9ee385b9e3aa5108c625ee375
Jari Ronkainen <ronchaine@gmail.com>
2017-07-04 11:06:03 +0300

    Add definitions for parsing buffers

--- extra/bufferparser.hpp
+++ extra/bufferparser.hpp
@@ -1,7 +1,17 @@
 #ifndef MUSH_BUFFERPARSER
 #define MUSH_BUFFERPARSER
 
+#include <tuple>
+#include "buffer.hpp"
+
 #ifdef MUSH_COMPRESSION
 #endif
 
+namespace mush
+{
+    template<typename... T>
+    std::tuple<T...> parse(const Buffer& buf)
+    {
+    }
+}
 #endif
