f5305d19e09d1bd864c9d38e71e2b53320b9830b
Jari Ronkainen <ronchaine@gmail.com>
2017-12-26 17:05:52 +0200

    Add preliminary distribution class

--- random.hpp
+++ random.hpp
@@ -5,6 +5,35 @@
 
 namespace mush
 {
+    template <typename ResultType>
+    class Distribution
+    {
+        typedef ResultType  result_type;
+
+        private:
+            template <bool Integers = std::is_integral<ResultType>::value>
+            class bounds_type
+            {
+                result_type a;
+                result_type b;
+
+                public:
+                    typedef Distribution distribution_type;
+
+                    template <bool Enable = Integers, std::enable_if_t<Enable, int> = 0>
+                    explicit bounds_type(result_type a = 0, result_type b = std::numeric_limits<result_type>::max())
+                        : a(a), b(b) {}
+                    
+                    template <bool Enable = Integers, std::enable_if_t<!Enable, int> = 0>
+                    explicit bounds_type(result_type a = 0.0, result_type b = 1.0)
+                        : a(a), b(b) {}
+
+                    result_type min() const { return a; }
+                    result_type max() const { return b; }
+            };
+        public:
+    };
+
     namespace detail
     {
         template <typename Generator = std::mt19937>
